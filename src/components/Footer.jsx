afe6169d659fbd44c9ce7d03db9a30d8228587d3
silvana02 <silvanalima.89@hotmail.com>
2024-05-15 20:28:45 +0200

    Refactor footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,35 +6,44 @@ import {
   LiaLinkedin,
   LiaYoutube,
 } from 'react-icons/lia'
+import { Link } from 'react-router-dom'
 
+import imageFooter from '../assets/image-footer.png'
+import logoAdaBlanco from '../assets/logo-Ada-blanco.png'
 import { IconLink } from './IconLink'
 
 export const Footer = () => {
   return (
-    <HStack bgColor={'#222222'} py={'32px'} justifyContent={'center'}>
+    <HStack justifyContent={'center'}>
       <Grid
         templateColumns={['1fr', '1fr', 'repeat(2, 1fr)']}
-        w={['100%', '100%', '1200px']}
+        w={['100%', '100%', '1440px']}
         alignItems={'center'}
-        gap={[8, 0]}
+        gap={[8, 8, 0]}
+        color={'#FFFFFF'}
+        bgColor={'#222222'}
+        py={'32px'}
+        px={'48px'}
       >
         <GridItem colSpan={1}>
-          <VStack alignItems={['center', 'flex-start']}>
-            <Image
-              boxSize={178}
-              h={77}
-              objectFit="cover"
-              src="/logo-blanco.svg"
-              alt="Logo Ada"
-            />
-            <Text color={'#FFFFFF'}>
+          <VStack alignItems={['center', 'center', 'flex-start']}>
+            <Link to={'/'}>
+              <Image
+                h={77}
+                objectFit="cover"
+                src={logoAdaBlanco}
+                alt="Logo Ada"
+              />
+            </Link>
+
+            <Text textAlign={['center', 'center', 'start']}>
               Empoderamos mujeres a través de la tecnología.
             </Text>
           </VStack>
         </GridItem>
-        <GridItem colSpan={1} justifySelf={'end'}>
+        <GridItem colSpan={1} justifySelf={['center', 'center', 'end']}>
           <VStack alignItems={'center'}>
-            <Image src="/image-footer.png" alt="Logo Ada" />
+            <Image src={imageFooter} alt="Logo Ada" />
             <HStack>
               <IconLink
                 href="https://www.instagram.com/adaitw/"
@@ -57,9 +66,11 @@ export const Footer = () => {
                 isExternal
               />
             </HStack>
-            <HStack color={'#FFFFFF'}>
+            <HStack>
               <LiaCopyright />
-              <Text>2024 Ada ITW, todos los derechos reservados.</Text>
+              <Text textAlign={'center'}>
+                2024 Ada ITW, todos los derechos reservados.
+              </Text>
             </HStack>
           </VStack>
         </GridItem>
--- src/components/IconLink.jsx
+++ src/components/IconLink.jsx
@@ -1,14 +1,19 @@
 import { Link } from '@chakra-ui/react'
+import PropTypes from 'prop-types'
 
 export const IconLink = ({ icon, ...props }) => {
   return (
     <Link
       color={'#d07efc'}
-      fontSize={'40px'}
+      fontSize={'35px'}
       _hover={{ color: '#eac3ff' }}
       {...props}
     >
       {icon}
     </Link>
   )
+}
+
+IconLink.propTypes = {
+  icon: PropTypes.any.isRequired,
 }
