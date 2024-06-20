87e8d2c79cb98d301be3516c1ef5f327d429e9a2
silvana02 <silvanalima.89@hotmail.com>
2024-06-20 12:33:47 +0200

    Refactor CoursesCard

--- src/components/home/CoursesCard.jsx
+++ src/components/home/CoursesCard.jsx
@@ -1,6 +1,8 @@
 import {
   Button,
   Card,
+  CardBody,
+  CardFooter,
   Divider,
   Heading,
   Hide,
@@ -29,124 +31,129 @@ export const CoursesCard = ({
     <Card
       w={['224px', '299px', '299px', '399px']}
       h={['340px', '420px', '420px', '520px']}
-      py={['spacingL.xl', 'spacingL.xl', 'spacingL.xl', 'spacingXl.base']}
+      py={['spacingM.xl', 'spacingL.xl', 'spacingL.xl', 'spacingXl.base']}
       px={['spacingM.xl', 'spacingM.xl', 'spacingM.xl', 'spacingXl.base']}
       overflow="hidden"
       color={'gray.600'}
     >
-      <Heading
-        as={'h4'}
-        fontFamily={'kMid'}
-        fontSize={['h4.base', 'h4.base', 'h4.base', 'h4.xl']}
-      >
-        {title}
-      </Heading>
-      <Divider
-        borderColor={'#222222'}
-        borderWidth={'1px'}
-        mb={'spacingXs.xl'}
-      />
-      {/* Carrera y descripcion */}
-      <Stack
-        h={{ base: '170px', md: '200px', lg: '300px' }}
-        py={['spacingXs.lg', 'spacingXs.lg', 'spacingXs.lg', 'spacingXs.xl']}
-      >
-        <Text
-          fontSize={[
-            'largeTxt.base',
-            'largeTxt.base',
-            'largeTxt.base',
-            'largeTxt.lg',
-          ]}
-          color={color}
-          fontWeight={'semibold'}
+      <CardBody p={0}>
+        <Heading
+          as={'h4'}
+          fontFamily={'kMid'}
+          fontSize={['h4.base', 'h4.base', 'h4.base', 'h4.xl']}
         >
-          {career}
-        </Text>
-        <Show below="lg">
-          <Text
-            fontSize={[
-              'midTxt1.base',
-              'midTxt1.base',
-              'midTxt1.base',
-              'midTxt1.xl',
-            ]}
-          >
-            {descriptionS}
-          </Text>
-        </Show>
-        <Hide below="lg">
-          <Text
-            fontSize={[
-              'midTxt1.base',
-              'midTxt1.base',
-              'midTxt1.base',
-              'midTxt1.xl',
-            ]}
-            textAlign={'justify'}
-          >
-            {descriptionL}
-          </Text>
-        </Hide>
-      </Stack>
-      <Stack
-        justify={'center'}
-        align={'flex-start'}
-        py={['spacingS.base', 'spacingS.base', 'spacingS.base', 'spacingL.xl']}
-      >
-        {/* iconos - fecha - duracion -financiacion */}
-        <Stack>
-          <HStack>
-            <Icon
-              icon="clarity:calendar-line"
-              color={color}
-              style={{
-                fontSize: `calc(${theme.fontSizes.h4.base} + 1vw)`,
-              }}
-            />
+          {title}
+        </Heading>
+        <Divider
+          borderColor={'#222222'}
+          borderWidth={'1px'}
+          mb={'spacingXs.xl'}
+        />
+        {/* Carrera y descripcion */}
+        <Stack
+          h={['250px', '250px', '300px', '367px']}
+          justifyContent={'space-between'}
+        >
+          <Stack>
             <Text
-              lineHeight="1.2"
-              fontSize={['smTxt.base', 'smTxt.base', 'smTxt.lg', 'smTxt.xl']}
-            >
-              {date}
-            </Text>
-          </HStack>
-          <HStack>
-            <Icon
-              icon="ph:clock"
+              fontSize={[
+                'largeTxt.base',
+                'largeTxt.base',
+                'largeTxt.base',
+                'largeTxt.lg',
+              ]}
               color={color}
-              style={{
-                fontSize: `calc(${theme.fontSizes.h4.base} + 1vw)`,
-              }}
-            />
-            <Text
-              lineHeight="1.2"
-              fontSize={['smTxt.base', 'smTxt.base', 'smTxt.lg', 'smTxt.xl']}
+              fontWeight={'semibold'}
             >
-              {duration}
+              {career}
             </Text>
-          </HStack>
-          <HStack>
-            <Icon
-              icon="pepicons-print:dollar"
-              color={color}
-              style={{
-                fontSize: `calc(${theme.fontSizes.h4.base} + 1vw)`,
-              }}
-            />
-            <Text
-              lineHeight="1.2"
-              fontSize={['smTxt.base', 'smTxt.base', 'smTxt.lg', 'smTxt.xl']}
-            >
-              Distintos métodos de financiación disponibles
-            </Text>
-          </HStack>
+            <Show below="lg">
+              <Text
+                fontSize={[
+                  'midTxt1.base',
+                  'midTxt1.base',
+                  'midTxt1.base',
+                  'midTxt1.xl',
+                ]}
+              >
+                {descriptionS}
+              </Text>
+            </Show>
+            <Hide below="lg">
+              <Text
+                fontSize={[
+                  'midTxt1.base',
+                  'midTxt1.base',
+                  'midTxt1.base',
+                  'midTxt1.xl',
+                ]}
+                textAlign={'justify'}
+              >
+                {descriptionL}
+              </Text>
+            </Hide>
+          </Stack>
+          {/* iconos - fecha - duracion -financiacion */}
+          <Stack pb={[3, 5]}>
+            <HStack>
+              <Icon
+                icon="clarity:calendar-line"
+                color={color}
+                style={{
+                  fontSize: `calc(${theme.fontSizes.h4.base} + 1vw)`,
+                }}
+              />
+              <Text
+                lineHeight="1.2"
+                fontSize={['smTxt.base', 'smTxt.base', 'smTxt.lg', 'smTxt.xl']}
+              >
+                {date}
+              </Text>
+            </HStack>
+            <HStack>
+              <Icon
+                icon="ph:clock"
+                color={color}
+                style={{
+                  fontSize: `calc(${theme.fontSizes.h4.base} + 1vw)`,
+                }}
+              />
+              <Text
+                lineHeight="1.2"
+                fontSize={['smTxt.base', 'smTxt.base', 'smTxt.lg', 'smTxt.xl']}
+              >
+                {duration}
+              </Text>
+            </HStack>
+            <HStack>
+              <Icon
+                icon="pepicons-print:dollar"
+                color={color}
+                style={{
+                  fontSize: `calc(${theme.fontSizes.h4.base} + 1vw)`,
+                }}
+              />
+              <Text
+                lineHeight="1.2"
+                fontSize={['smTxt.base', 'smTxt.base', 'smTxt.lg', 'smTxt.xl']}
+              >
+                Distintos métodos de financiación disponibles
+              </Text>
+            </HStack>
+          </Stack>
         </Stack>
-      </Stack>
+      </CardBody>
 
-      <Button variant="button-secondary" size={'md'} w={'100%'}>
-        Inscríbete ahora
-      </Button>
+      <CardFooter p={0}>
+        <Button
+          variant="button-secondary"
+          size={'md'}
+          w={'100%'}
+          h={['24px', '38px']}
+        >
+          Inscríbete ahora
+        </Button>
+      </CardFooter>
     </Card>
   )
 }
