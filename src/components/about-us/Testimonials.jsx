08b544284ca9541b8ceebc264c42e9b465d9ea73
silvana02 <silvanalima.89@hotmail.com>
2024-07-04 00:39:36 +0200

    Refactored testimonial sections

--- src/components/about-us/Testimonials.jsx
+++ src/components/about-us/Testimonials.jsx
@@ -1,4 +1,4 @@
-import { Container, Grid, Heading, Show, Text } from '@chakra-ui/react'
+import { Container, Grid, Heading, Hide, Show, Text } from '@chakra-ui/react'
 
 import { studentTestimonials } from '../../utils/constants'
 import { TestimonyCard } from '../home/testimonials/TestimonyCard'
@@ -8,7 +8,7 @@ export const Testimonials = () => {
     <Container maxW={'100%'} bg={'gray.200'} centerContent>
       <Container
         maxW={['100%', '1344px']}
-        //  px={['spacingM.base', 'spacingM.base', 'spacingM.base', 'spacingXl.xl']}
+        px={['spacingM.base', 'spacingM.base', 'spacingM.base', 'spacingXl.xl']}
         py={['spacingXl.lg', 'spacingXl.lg', 'spacingXl.lg', '80px']}
       >
         <Show below="lg">
@@ -46,26 +46,53 @@ export const Testimonials = () => {
         </Show>
         <Container maxW={'1164px'} centerContent>
           <Grid
-            templateColumns={['repeat(1, 1fr)', 'repeat(2, 1fr)']}
-            templateRows={['repeat(2, 1fr)', 'repeat(1, 1fr)']}
+            templateColumns={[
+              'repeat(1, 1fr)',
+              'repeat(1, 1fr)',
+              'repeat(1, 1fr)',
+              'repeat(2, 1fr)',
+            ]}
+            templateRows={[
+              'repeat(2, 1fr)',
+              'repeat(2, 1fr)',
+              'repeat(2, 1fr)',
+              'repeat(1, 1fr)',
+            ]}
             gap={'40px'}
           >
-            {studentTestimonials.map(
-              ({ name, career, comment, photo, id, enterprise }) => {
-                if (enterprise) {
-                  return (
-                    <TestimonyCard
-                      key={id}
-                      comment={comment}
-                      name={name}
-                      career={career}
-                      photo={photo}
-                    />
-                  )
+            <Hide below="lg">
+              {studentTestimonials.map(
+                ({ name, career, comment, photo, id, enterprise }) => {
+                  if (enterprise) {
+                    return (
+                      <TestimonyCard
+                        key={id}
+                        comment={comment}
+                        name={name}
+                        career={career}
+                        photo={photo}
+                        h={['345px', '260px', '300px', '400px']}
+                      />
+                    )
+                  }
+                  return null
                 }
-                return null
-              }
-            )}
+              )}
+            </Hide>
+            <Show below="lg">
+              {studentTestimonials
+                .slice(0, 4)
+                .map(({ name, career, comment, photo, id }) => (
+                  <TestimonyCard
+                    key={id}
+                    comment={comment}
+                    name={name}
+                    career={career}
+                    photo={photo}
+                    h={['330px', '300px', '300px', '420px']}
+                  />
+                ))}
+            </Show>
           </Grid>
         </Container>
       </Container>
